d6c089874e626c0cd39d5b45ff9063b3cfe395ef
wangwei <w18234850529@163.com>
2023-02-14 11:21:21 +0800

    【购物车服务】合并

--- mallsystem-service/mallsystem-service-cart/src/main/java/com/wangwei/mall/cart/service/impl/CartServiceImpl.java
+++ mallsystem-service/mallsystem-service-cart/src/main/java/com/wangwei/mall/cart/service/impl/CartServiceImpl.java
@@ -68,27 +68,77 @@ public class CartServiceImpl implements CartService {
     @Override
     public List<CartInfo> getCartList(String userId, String userTempId) {
 
-        //创建返回结果集
-        List<CartInfo> result = null;
+        //未登录临时购物车
+        List<CartInfo> noLoginCartInfoList = null;
 
-        if (!StringUtils.isEmpty(userId)){
-            BoundHashOperations boundHashOperations = redisTemplate.boundHashOps(getCartKey(userId));
-            result = boundHashOperations.values();
-        }
 
+        //情况一：未登录，获取临时购物车
         if (!StringUtils.isEmpty(userTempId)){
-            BoundHashOperations boundHashOperations = redisTemplate.boundHashOps(getCartKey(userTempId));
-            result = boundHashOperations.values();
+            //获取cartKey
+            String cartKey = getCartKey(userTempId);
+            BoundHashOperations<String,String,CartInfo> boundHashOperations = redisTemplate.boundHashOps(cartKey);
+            //获取商品列表
+            noLoginCartInfoList = boundHashOperations.values();
         }
 
-        if (!CollectionUtils.isEmpty(result)){
-            result.sort((o1,o2) -> {
-                //使用时间进行比较
-                return DateUtil.truncatedCompareTo(o2.getUpdateTime(), o1.getUpdateTime(), Calendar.SECOND);
+        /**
+         * 判断确认未登录,如果未登录,排序后直接返回
+         */
+        if (StringUtils.isEmpty(userId)){
+           //排序
+           if (!CollectionUtils.isEmpty(noLoginCartInfoList)){
+               noLoginCartInfoList.sort((o1,o2) -> {
+                   return DateUtil.truncatedCompareTo(o1.getUpdateTime(), o2.getUpdateTime(), Calendar.SECOND);
+               });
+           }
+           return noLoginCartInfoList;
+        }
+
+        //情况2：用户已经登录
+        //获取登录后用户的购物车
+        BoundHashOperations<String,String,CartInfo> operations = redisTemplate.boundHashOps(getCartKey(userId));
+        //判断未登录时购物车里是否有商品
+        if (!CollectionUtils.isEmpty(noLoginCartInfoList)){
+            //如果临时购物车有商品
+            noLoginCartInfoList.stream().forEach(cartInfo -> {
+                if (operations.hasKey(cartInfo.getSkuId().toString())){
+
+                    //获取登录后的购物信息
+                    CartInfo loginCartInfo = operations.get(cartInfo.getSkuId().toString());
+                    //数量相加
+                    loginCartInfo.setSkuNum(cartInfo.getSkuNum()+loginCartInfo.getSkuNum());
+                    //更新修改时间
+                    loginCartInfo.setUpdateTime(new Date());
+                    //更新价格
+                    loginCartInfo.setSkuPrice(productService.getSkuPrice(loginCartInfo.getSkuId()));
+                    //判断选中
+                    if (cartInfo.getIsChecked().intValue()==1){
+                        loginCartInfo.setIsChecked(1);
+                    }
+                    //修改redis中登录后购物车的数据
+                    operations.put(cartInfo.getSkuId().toString(), loginCartInfo);
+
+                }else {
+                    //表示不包含
+                    cartInfo.setUserId(userId);
+                    cartInfo.setCreateTime(new Date());
+                    cartInfo.setUpdateTime(new Date());
+                    operations.put(cartInfo.getSkuId().toString(),cartInfo);
+                }
             });
+
+            //表示合并完成
+            redisTemplate.delete(getCartKey(userTempId));
         }
 
-        return null;
+        //获取登录成功的购物车
+        List<CartInfo> loginCartInfoList = operations.values();
+        if (!CollectionUtils.isEmpty(loginCartInfoList)){
+            return loginCartInfoList;
+        }else {
+            return new ArrayList<>();
+        }
+
     }
 
     /**
