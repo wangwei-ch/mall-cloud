6e155a08858136e6e0fbc7c13c14d0e84fcbe363
wangwei <w18234850529@163.com>
2023-02-27 11:43:11 +0800

    【定时任务服务】定时任务服务搭建

---
+++ mallsystem-service/mallsystem-service-task/src/main/java/com/wangwei/mall/task/MallServiceTaskApplication.java
@@ -0,0 +1,16 @@
+package com.wangwei.mall.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//取消数据源自动配置
+@ComponentScan({"com.wangwei.mall"})
+@EnableDiscoveryClient
+public class MallServiceTaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallServiceTaskApplication.class, args);
+    }
+}
